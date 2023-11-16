9647ccc91b714b65ce4894d9330a25b044ec64bf
NguyenLeBaQuoc <baby.quocnguyen2@gmail.com>
2023-11-16 11:16:01 +0700

    Project Báo Cáo

---
+++ views/frontend/footer.php
@@ -0,0 +1,66 @@
+<section class="hdl-footer pb-4">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-4 pt-4">
+            <h3 class="widgettilte text-main">CHÚNG TÔI LÀ AI ?</h3>
+               <p class="pt-1">
+                  Copyright@ 2024 Shop78 là hệ thống bán sỉ và lẻ thời trang nam, nữ, trẻ em và quần áo thể thao,
+                  mong muốn đem đến chất lượng tốt nhất cho khách hàng.
+               </p>
+               <p class="pt-1">
+                  Địa chỉ: 123 Hoàng Diệu 2, Linh Trung, Thủ Đức, TP.HCM
+               </p>
+               <p class="pt-1">
+                  Điện thoại: 0935465731(call, zalo) - Email: baby.quocnguyen2@gmail.com
+               </p>
+            <h3 class="widgettilte text-main">MẠNG XÃ HỘI CỦA CHÚNG TÔI</h3>
+            <div class="social my-3">
+               <div class="facebook-icon">
+                  <a href="https://www.facebook.com/731.baquoc/">
+                     <i class="fab fa-facebook-f"></i>
+                  </a>
+               </div>
+               <div class="instagram-icon">
+                  <a href="https://www.instagram.com/cuoctretrau/?fbclid=IwAR14qkDMy2WrEIprZUpVtrorKk6B12szg7mi9WZjJCjU93JChtQROebw_ak">
+                     <i class="fab fa-instagram"></i>
+                  </a>
+               </div>
+               <div class="google-icon">
+                  <a href="https://www.google.com.vn/?hl=vi">
+                     <i class="fab fa-google"></i>
+                  </a>
+               </div>
+               <div class="youtube-icon">
+                  <a href="https://www.youtube.com/">
+                     <i class="fab fa-youtube"></i>
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="col pt-4 ">
+         <iframe class="img-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.7609399213206!2d106.77063267462792!3d10.829597989322405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752701cbacce21%3A0xc55b53936092d0e1!2zS8O9IFTDumMgWMOhIFRyxrDhu51uZyBDYW8gxJDhurNuZyBDw7RuZyBUaMawxqFuZyBUUC5IQ00!5e0!3m2!1svi!2s!4v1699500845428!5m2!1svi!2s"
+                width="500" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+           
+         </div>
+        
+         <div class="col pt-4 text-main">
+
+         <?php require_once "views/frontend/mod-footermenu.php" ?>
+                <div class="">
+               <strong>Lượt Truy Cập Vào Website</strong>
+            </h3>
+            <p class="my-1">2975739 lượt</p>
+            </div>
+         </div>
+       
+      </div>
+   </div>
+</section>
+<section class="hdl-copyright bg-dark py-3">
+   <div class="container text-center text-white">
+   Thiết kế bởi: <span style="font-weight: bold;">Thái Tử Phú Yên. </span> - Phone: 0935465731
+   </div>
+</section>
+</body>
+
+</html>
